47b8bb68660c3cb330459e502f1bf959813ea310
wa1k-cyber <56413301+wa1k-cyber@users.noreply.github.com>
2020-02-13 13:49:01 -0800

    added the backround scrolling script

---
+++ Mobile Group Project 2/Assets/TDtesting/backscroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backscroll : MonoBehaviour
+{
+    Material mat;
+    Vector2 offset;
+    public int xVelocity, yVelocity;
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+        mat.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
