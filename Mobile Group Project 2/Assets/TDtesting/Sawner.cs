90cc671b0c72156e00848f3c966fd8dfc293aa53
wa1k-cyber <56413301+wa1k-cyber@users.noreply.github.com>
2020-02-18 12:13:06 -0800

    add scripts i was working on back in

---
+++ Mobile Group Project 2/Assets/TDtesting/Sawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sawner : MonoBehaviour
+{
+    public GameObject obstacle;
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (timeBtwSpawn <= 0)
+        {
+            Instantiate(obstacle, transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
